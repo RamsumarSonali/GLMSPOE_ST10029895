f3d04a3d86588b214f5a6d4443f578b9c2807530
RamsumarSonali <ST10029895@imconnect.edu.za>
2026-04-21 20:26:26 +0200

    second test push
    First push showing declarations didnt show on git

--- GLMS_Part2/obj/Debug/net9.0/GLMS_Part2.AssemblyInfo.cs
+++ GLMS_Part2/obj/Debug/net9.0/GLMS_Part2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GLMS_Part2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a797457315372bce1294a17ecaecb383b4816a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GLMS_Part2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GLMS_Part2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
